6f9d05e386148a5d87d06e025cd7b9a638b9fbed
André Wünscher <finthor@gmail.com>
2024-04-05 14:00:20 +0200

    add to namespace fulldome / remove unneeded using directives

--- projects/DomeViewer/Assets/Modules/FileSystem/OBBHandler.cs
+++ projects/DomeViewer/Assets/Modules/FileSystem/OBBHandler.cs
@@ -1,83 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
-public class OBBHandler : MonoBehaviour
-{   
-/*
-    #################################################################################################################
-    Mediafiles need to be in this location on Android (via sideload or via deployment with Meta Quest Developer Hub):
-    sdcard/Android/oob/com.example.myapp/ as *.jpg / *.png / *.mp4
-    #################################################################################################################
-*/
+namespace pfc.Fulldome
+{
 
-    void Start()
-    {
-        #if UNITY_ANDROID && !UNITY_EDITOR
-        // Sourcepath OBB-Folder
-        string obbFolderPath = "/sdcard/Android/obb/"+Application.identifier;
+    public class OBBHandler : MonoBehaviour
+    {   
+    /*
+        #################################################################################################################
+        Mediafiles need to be in this location on Android (via sideload or via deployment with Meta Quest Developer Hub):
+        sdcard/Android/oob/com.example.myapp/ as *.jpg / *.png / *.mp4
+        #################################################################################################################
+    */
 
-        // Targetpath on Android Device
-        string destinationPath = "/sdcard/Download/DomeViewer";
-
-        try
-            {
-                System.IO.Directory.CreateDirectory(GetStoragePath());
-            }
-        catch (System.Exception e)
-            {
-                Debug.LogError(e.Message);
-            }
-        
-        try
+        void Start()
         {
-            // checkk obb folder existence
-            if (Directory.Exists(obbFolderPath))
-            {
-                // list all files in obb folder
-                string[] obbFiles = Directory.GetFiles(obbFolderPath);
+            #if UNITY_ANDROID && !UNITY_EDITOR
+            // Sourcepath OBB-Folder
+            string obbFolderPath = "/sdcard/Android/obb/"+Application.identifier;
 
-                // move all files
-                foreach (string obbFile in obbFiles)
+            // Targetpath on Android Device
+            string destinationPath = "/sdcard/Download/DomeViewer";
+
+            try
                 {
-                    // get filename
-                    string fileName = Path.GetFileName(obbFile);
-
-                    // move current file to target location
-                    File.Move(obbFile, Path.Combine(destinationPath, fileName));
+                    System.IO.Directory.CreateDirectory(GetStoragePath());
                 }
-
-                Debug.Log("All files are moved successfully.");
-            }
-            else
+            catch (System.Exception e)
+                {
+                    Debug.LogError(e.Message);
+                }
+            
+            try
             {
-                Debug.LogError("OBB-Folder not found");
+                // checkk obb folder existence
+                if (Directory.Exists(obbFolderPath))
+                {
+                    // list all files in obb folder
+                    string[] obbFiles = Directory.GetFiles(obbFolderPath);
+
+                    // move all files
+                    foreach (string obbFile in obbFiles)
+                    {
+                        // get filename
+                        string fileName = Path.GetFileName(obbFile);
+
+                        // move current file to target location
+                        File.Move(obbFile, Path.Combine(destinationPath, fileName));
+                    }
+
+                    Debug.Log("All files are moved successfully.");
+                }
+                else
+                {
+                    Debug.LogError("OBB-Folder not found");
+                }
             }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Error moving OBB-Files: " + e.Message);
+            }
+            #else
+            Debug.Log("Move obb files skipped");
+            #endif
         }
-        catch (System.Exception e)
-        {
-            Debug.LogError("Error moving OBB-Files: " + e.Message);
-        }
-        #else
-        Debug.Log("Move obb files skipped");
-        #endif
+        private string GetStoragePath()
+            {
+                string path = "/sdcard/Download/DomeViewer";
+
+
+    #if UNITY_ANDROID && !UNITY_EDITOR
+            // AndroidJavaClass Environment = new AndroidJavaClass("android.os.Environment");
+            // AndroidJavaObject directory = Environment.CallStatic<AndroidJavaObject>("getExternalStoragePublicDirectory", "Pictures");
+            // path = directory.Call<string>("getAbsolutePath");
+            // path = System.IO.Path.Combine(path, "Oculus");
+            // path = System.IO.Path.Combine(path, "Gallery");
+    #else
+                path = Application.persistentDataPath + "/DomeViewer";
+    #endif
+
+                return path;
+            }
     }
-    private string GetStoragePath()
-        {
-            string path = "/sdcard/Download/DomeViewer";
-
-
-#if UNITY_ANDROID && !UNITY_EDITOR
-        // AndroidJavaClass Environment = new AndroidJavaClass("android.os.Environment");
-        // AndroidJavaObject directory = Environment.CallStatic<AndroidJavaObject>("getExternalStoragePublicDirectory", "Pictures");
-        // path = directory.Call<string>("getAbsolutePath");
-        // path = System.IO.Path.Combine(path, "Oculus");
-        // path = System.IO.Path.Combine(path, "Gallery");
-#else
-            path = Application.persistentDataPath + "/DomeViewer";
-#endif
-
-            return path;
-        }
 }
