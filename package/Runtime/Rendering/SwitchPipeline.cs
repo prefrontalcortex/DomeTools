77e51580906c1cc0032f5b5060e73ae4319096a2
Robert Dorn <dorn@prefrontalcortex.de>
2024-09-05 09:58:15 +0200

    unity api changes > GraphicSettings.defaultRenderPipeline

--- package/Runtime/Rendering/SwitchPipeline.cs
+++ package/Runtime/Rendering/SwitchPipeline.cs
@@ -11,7 +11,7 @@ namespace pfc.DomeTools
         // Start is called before the first frame update
         void OnEnable()
         {
-            GraphicsSettings.renderPipelineAsset = asset;
+            GraphicsSettings.defaultRenderPipeline = asset;
         }
     }
 }
