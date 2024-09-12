109bde7983efbe993d7a882412e79cbb2e9a5759
Felix Herbst <felix.herbst@gmail.com>
2024-09-12 12:41:43 +0200

    add local package exporter

---
+++ projects/DomeTools-2022.3/Assets/Editor/LocalPackageExporter.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace pfc.DomeTools
+{
+    public static class LocalPackageExporter
+    {
+        private static string PackageName = "com.pfc.dome-tools";
+
+        [MenuItem("Dome Tools/Development/Export Local Package")]
+        private static void ExportMenu()
+        {
+            ExportLocalPackage();
+        }
+        
+        private static string GetRootPath()
+        {
+            return Path.GetFullPath(Path.Combine("Assets", "..", "..", "DomeTools-2022.3-Immutable", "Packages"));
+        }
+        
+        private static async void ExportLocalPackage()
+        {
+            var rootPath = GetRootPath();
+            var packageFolder = Path.GetFullPath("Packages/" + PackageName);
+            
+            // Rename Samples to Samples~ in packageFolder
+            var samplesFolder = Path.Combine(packageFolder, "Samples");
+            var samplesFolderNew = Path.Combine(packageFolder, "Samples~");
+            if (Directory.Exists(samplesFolder))
+            {
+                if (Directory.Exists(samplesFolderNew)) Directory.Delete(samplesFolderNew, true);
+                Directory.Move(samplesFolder, samplesFolderNew);
+                File.Move(samplesFolder + ".meta", samplesFolderNew + ".meta");
+            }
+            
+            var targetFolder = Path.GetFullPath(rootPath);
+            Debug.Log("Packing " + packageFolder + " → " + targetFolder);
+            var packRequest = Client.Pack(packageFolder, targetFolder);
+            var prog = Progress.Start("Exporting Dome Tools Package", "Please wait...", Progress.Options.Indefinite);
+            while (!packRequest.IsCompleted)
+            {
+                Progress.Report(prog, .5f);
+                await Task.Yield();
+            }
+            if (packRequest.Status == StatusCode.Success)
+            {
+                Progress.Finish(prog, Progress.Status.Succeeded);
+                var tarball = packRequest.Result.tarballPath;
+                var target = Path.GetDirectoryName(tarball) + "/pfc-dome-tools.tgz";
+                if (File.Exists(target)) File.Delete(target);
+                File.Move(tarball, target);
+                var fileSize = File.Exists(target) ? new FileInfo(target).Length : 0;
+                var fileSizeMb = $"{fileSize / 1024f / 1024f:F1} MB";
+                Debug.Log($"Success → {target} ({fileSizeMb})");
+                EditorUtility.RevealInFinder(target); 
+            }
+            else
+            {
+                Progress.Finish(prog, Progress.Status.Failed);
+                Debug.LogError("Failed to pack " + packageFolder + " → " + targetFolder);
+            }
+            
+            // Rename Samples~ back to Samples in packageFolder
+            if (Directory.Exists(samplesFolderNew))
+            {
+                if (Directory.Exists(samplesFolder)) Directory.Delete(samplesFolder, true);
+                Directory.Move(samplesFolderNew, samplesFolder);
+                File.Move(samplesFolderNew + ".meta", samplesFolder + ".meta");
+            }
+        }
+    }
+}
